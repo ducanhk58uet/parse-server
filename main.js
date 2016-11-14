f7daa982b6d340bfa3e7de0a4b5556c967393cf8
Duc Anh <ducanh54@gmail.com>
2016-11-14 21:03:09 +0700

    update config main

--- main.js
+++ main.js
@@ -2,18 +2,13 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
-
-if (!databaseUri) {
-    console.log('DATABASE_URI not specified, falling back to localhost.');
-}
-
 var api = new ParseServer({
-    databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-    cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-    appId: process.env.APP_ID || 'snapsofts123',
-    masterKey: process.env.MASTER_KEY || '123456', //Add your master key here. Keep it secret!
-    serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+    databaseURI: 'mongodb://localhost:27017/dev', // Connection string for your MongoDB database
+    cloud: 'cloud/main.js', // Absolute path to your Cloud Code
+    appId: 'snapsofts123',
+    masterKey: '123456', // Keep this key secret!
+    fileKey: 'optionalFileKey',
+    serverURL: 'http://localhost:1337/parse', // Don't forget to change to https if needed
     liveQuery: {
         classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
     }
@@ -28,8 +23,7 @@ var app = express();
 app.use('/public', express.static(path.join(__dirname, '/public_html')));
 
 // Serve the Parse API on the /parse URL prefix
-var mountPath = process.env.PARSE_MOUNT || '/parse';
-app.use(mountPath, api);
+app.use('/parse', api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
